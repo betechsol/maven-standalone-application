bff77924893a3586013c121758bdc2fdb67999a4
Betech Devops <betechincorporated@gmail.com>
2025-05-20 20:41:30 +0000

    updated scripts

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,12 +9,12 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Engineers, Welcome to Landmark Technologies DevOps Master Class. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Engineers, Welcome to BETECH Solutions DevOps Master Class. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
 	private static Date getLocalCurrentDate() {
-		String m = "Landmark Technologies";
+		String m = "BETECH Solutions";
 		return new Date();
 
 	}
